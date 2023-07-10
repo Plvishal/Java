d7e9dc03c743846acafbf809b601f09149265b5d
Vishal <vishalpal1501@gmail.com>
2023-07-10 22:53:40 +0530

    sum of a & b

---
+++ variable_Data_types/sumOftwonumber.java
@@ -0,0 +1,11 @@
+package variable_Data_types;
+
+public class sumOftwonumber {
+     public static void main(String[] args) {
+        int a =10;
+        int b=45;
+        int sum = a+b;
+        
+        System.out.println("Sum of a&b is ="+sum);
+    }
+}
