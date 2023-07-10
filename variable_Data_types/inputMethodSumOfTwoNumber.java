fa6cd553392f7370ef5c17416e7c65ce64204847
Vishal <vishalpal1501@gmail.com>
2023-07-10 23:20:24 +0530

    Sum of two number take input from user

---
+++ variable_Data_types/inputMethodSumOfTwoNumber.java
@@ -0,0 +1,15 @@
+package variable_Data_types;
+import java.util.*;
+
+public class inputMethodSumOfTwoNumber {
+    public static void main(String[] args) {
+     Scanner sc= new Scanner(System.in);
+    System.out.print("Enter the first number:");
+    int a =sc.nextInt();
+    System.out.print("Enter the second number:");
+     int b =sc.nextInt();
+    int sum = a+b;
+    System.out.println("Sum of two number is="+sum);
+        
+    }
+}
