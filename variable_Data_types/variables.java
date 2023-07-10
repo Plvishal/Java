4d66bae9c88769c363b67ed03f1915fc74ca2098
Vishal <vishalpal1501@gmail.com>
2023-07-10 22:24:58 +0530

    Initial Commit for the variables

---
+++ variable_Data_types/variables.java
@@ -0,0 +1,12 @@
+package variable_Data_types;
+
+public class variables {
+    public static void main(String args[]){
+        int a = 30;
+        int b= 44;
+        String name="Vishal Pal";   
+     System.out.println(a);
+     System.out.println(b);
+     System.out.println(name);
+    }
+}
