33ac1c284918f48d3c09d6b3d81acfda5783efae
Vishal <vishalpal1501@gmail.com>
2023-08-07 23:57:48 +0530

    Print the largest of two numbers

--- ConditionalStatement/ifElseStatement.java
+++ ConditionalStatement/ifElseStatement.java
@@ -2,7 +2,7 @@ package ConditionalStatement;
 import java.util.*;
 public class ifElseStatement {
     public static void main(String args[]){
-        Scanner  sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter your age  ::=> ::=");
         int age = sc.nextInt();
 
