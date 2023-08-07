483f651bb871b88a829dc7029b9695ae4ae8fe52
Vishal <vishalpal1501@gmail.com>
2023-08-07 22:44:43 +0530

    Arithmetic Operators

---
+++ oprators/arithmeticOperators.java
@@ -0,0 +1,13 @@
+package oprators;
+
+public class arithmeticOperators {
+    public static void main(String args[]){
+int A=15;
+int B= 12;
+System.out.println("Addition of two number =" +(A+B));
+System.out.println("Subtraction of two number =" +(A-B));
+System.out.println("Multification of two number =" +(A*B));
+System.out.println("Divide of two number =" +(A/B));
+System.out.println("Remainder of two number =" +(A%B));
+    }
+}
